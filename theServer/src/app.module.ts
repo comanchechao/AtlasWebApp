c08c33bfaaf2c618312a29ea20e2c3e9ebc0ba69
Greez <imgreez@protonmail.com>
2023-07-04 16:25:48 +0330

    modules implementation

--- theServer/src/app.module.ts
+++ theServer/src/app.module.ts
@@ -1,9 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ExamModule } from './exam/exam.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ExamModule],
   controllers: [AppController],
   providers: [AppService],
 })
