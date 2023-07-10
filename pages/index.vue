bbe065462aba0f888ccc4c48c62a7691d69cfea9
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-07-11 01:17:36 +0330

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,19 @@
     <Navbar />
     <div class="w-full h-auto justify-center flex-col flex items-center">
       <img src="../assets/images/Wave1.webp" class="w-full" alt="" />
-      <div class="flex items-center justify-around w-full h-full p-10">
+      <div class="flex items-center justify-around w-full h-full px-32">
         <img
           src="../assets/images/MainPage.webp"
           class="h-rem34 object-contain"
           alt=""
         />
         <div class="flex flex-col items-center space-y-7">
-          <h2 class="text-lg">به</h2>
-          <h1 class="text-5xl font-bold text-mainBlue">
-            موسسه آموزشی و فرهنگی اصلس
+          <h2 class="text-xl text-darkBlue">موسسه آموزشی و فرهنگی</h2>
+          <h1
+            class="text-7xl border-b-4 rounded-lg pb-7 border-mainBlue font-bold text-mainBlue"
+          >
+            اصلس
           </h1>
-          <h2 class="text-lg">خوش اومدید</h2>
           <button
             class="px-12 py-3 text-xl border-2 items-center border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-md"
           >
