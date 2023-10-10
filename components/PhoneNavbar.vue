a88705c29aeac6363ae9f12dc57a80efe6aeac2f
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-10-10 06:37:27 +0330

    Update PhoneNavbar.vue

--- components/PhoneNavbar.vue
+++ components/PhoneNavbar.vue
@@ -2,11 +2,11 @@
   <div class="">
     <Sidebar position="right" v-model:visible="visible">
       <div
-        class="h-full w-full bg-darkPurple flex flex-col items-center justify-start space-y-5 px-16 py-10"
+        class="h-full w-full bg-darkPurple flex flex-col items-center justify-start space-y-5 px-16 py-20"
       >
         <NuxtLink class="w-full" to="/">
           <button
-            class="w-full justify-center py-1 border-2 items-center border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
+            class="w-full justify-center py-2 border-2 items-center border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
           >
             <span>خانه</span>
             <PhHouse :size="20" weight="fill" />
@@ -15,7 +15,7 @@
 
         <NuxtLink class="w-full" to="/exam">
           <button
-            class="w-full justify-center py-1 border-2 items-center border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
+            class="w-full justify-center py-2 border-2 items-center border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
           >
             <span>آزمون خلاقیت</span>
             <PhExam :size="20" weight="fill" />
@@ -23,7 +23,7 @@
         </NuxtLink>
         <NuxtLink class="w-full" to="/articles">
           <button
-            class="w-full justify-center py-1 border-2 border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
+            class="w-full justify-center py-2 border-2 border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
           >
             <span>مقالات</span>
             <PhArticle :size="20" weight="fill" />
@@ -31,7 +31,7 @@
         </NuxtLink>
         <NuxtLink class="w-full" to="/videoPlayer">
           <button
-            class="w-full justify-center py-1 border-2 border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
+            class="w-full justify-center py-2 border-2 border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
           >
             <span>ویدیوهای آموزشی</span>
             <PhVideo :size="20" weight="fill" />
@@ -39,7 +39,7 @@
         </NuxtLink>
         <NuxtLink class="w-full" to="/schedule">
           <button
-            class="w-full justify-center py-1 border-2 border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
+            class="w-full justify-center py-2 border-2 border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
           >
             <span>برنامه کلاسی</span>
             <PhCalendar :size="20" weight="fill" />
@@ -47,7 +47,7 @@
         </NuxtLink>
         <NuxtLink class="w-full" to="/admin">
           <button
-            class="w-full justify-center py-1 border-2 border-mainYellow text-md active:bg-mainYellow active:text-mainWhite bg-mainYellow hover:bg-mainWhite hover:text-mainYellow shadow-md shadow-transparent hover:shadow-mainBlue text-darkBlue transition ease-linear duration-200 flex space-x-2 rounded-sm"
+            class="w-full justify-center py-2 border-2 border-mainYellow text-md active:bg-mainYellow active:text-mainWhite bg-mainYellow hover:bg-mainWhite hover:text-mainYellow shadow-md shadow-transparent hover:shadow-mainBlue text-darkBlue transition ease-linear duration-200 flex space-x-2 rounded-sm"
           >
             <span>مدیریت</span>
             <PhGlobeStand :size="20" weight="fill" />
@@ -55,7 +55,7 @@
         </NuxtLink>
         <NuxtLink class="w-full" to="/contactUs">
           <button
-            class="w-full justify-center py-1 border-2 items-center border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
+            class="w-full justify-center py-2 border-2 items-center border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
           >
             <span>تماس با ما</span>
             <PhPhoneCall :size="20" weight="fill" />
@@ -63,7 +63,7 @@
         </NuxtLink>
         <NuxtLink class="w-full" to="/aboutUs">
           <button
-            class="w-full justify-center py-1 border-2 items-center border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
+            class="w-full justify-center py-2 border-2 items-center border-mainBlue text-md active:bg-mainBlue active:text-mainWhite bg-mainBlue hover:bg-mainWhite hover:text-mainBlue shadow-md shadow-transparent hover:shadow-mainBlue text-mainWhite transition ease-linear duration-200 flex space-x-2 rounded-sm"
           >
             <span>درباره ما</span>
             <PhTranslate :size="20" weight="fill" />
@@ -72,7 +72,7 @@
       </div>
     </Sidebar>
     <button
-      class="text-xl active:text-darkPurple active:bg-mainBlue flex items-center space-x-2 px-3 py-1 transition duration-300 ease-in-out border-2 border-transparent hover:border-darkBlue rounded-sm shadow-md shadow-transparent hover:shadow-darkBlue hover:text-darkBlue text-mainBlue"
+      class="text-xl active:text-darkPurple active:bg-mainBlue flex items-center space-x-2 px-3 py-2 transition duration-300 ease-in-out border-2 border-transparent hover:border-darkBlue rounded-sm shadow-md shadow-transparent hover:shadow-darkBlue hover:text-darkBlue text-mainBlue"
       @click="visible = true"
     >
       <PhList :size="40" />
