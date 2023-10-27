a4797d6969531c7844437e96e72824d4bd352be6
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-10-27 17:12:38 +0330

    aboutUs Update

--- pages/aboutUs.vue
+++ pages/aboutUs.vue
@@ -46,42 +46,302 @@
             معرفی پرسنل اطلس
           </h1>
           <div
-            class="lg:flex-row flex-col flex items-center justify-center space-y-5 lg:space-y-0 lg:space-x-6"
+            class="grid w-full grid-cols-1 lg:grid-cols-4 place-items-center gap-10 gap-y-14"
           >
             <div
+              class="flex flex-col w-full px-7 items-center justify-center lg:col-span-4"
+            >
+              <div class="w-44 h-44 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Pashaei.webp"
+                  class="object-obtain w-full"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg font-bold text-darkBlue my-4">طاهره پاشایی</h2>
+              <h2 class="text-xl border-b-8 border-mainYellow pb-2 rounded-xl">
+                مدیر و بنیانگذار
+              </h2>
+            </div>
+            <h1
+              class="lg:text-3xl lg:col-span-4 mb-3 font-bold text-xl place-self-end text-darkBlue border-b-8 border-mainYellow pb-4 rounded-xl"
+            >
+              مدرسان زبان اطلس
+            </h1>
+            <div
               class="flex flex-col w-full px-7 items-center justify-center space-y-6"
             >
-              <div class="w-36 h-36 rounded-full bg-mainBlue"></div>
-              <h2 class="text-md">
-                لوزم ایپسوم متنی است که اختراع شده تا جاهای خالی در طراحی گرافیک
-                پر شود و خالی نمایند متشکرم ازتون لوزم ایپسوم متنی است که اختراع
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Fatemeh.webp"
+                  class="object-obtain w-32 transform translate-x-1"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                فاطمه محمدپور
               </h2>
             </div>
             <div
               class="flex flex-col w-full px-7 items-center justify-center space-y-6"
             >
-              <div class="w-36 h-36 rounded-full bg-mainBlue"></div>
-              <h2 class="text-md">
-                لوزم ایپسوم متنی است که اختراع شده تا جاهای خالی در طراحی گرافیک
-                پر شود و خالی نمایند متشکرم ازتون لوزم ایپسوم متنی است که اختراع
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Nahid.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                ناهید نیا
               </h2>
             </div>
             <div
               class="flex flex-col w-full px-7 items-center justify-center space-y-6"
             >
-              <div class="w-36 h-36 rounded-full bg-mainBlue"></div>
-              <h2 class="text-md">
-                لوزم ایپسوم متنی است که اختراع شده تا جاهای خالی در طراحی گرافیک
-                پر شود و خالی نمایند متشکرم ازتون لوزم ایپسوم متنی است که اختراع
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Soda.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                سودا درویشی
               </h2>
             </div>
             <div
               class="flex flex-col w-full px-7 items-center justify-center space-y-6"
             >
-              <div class="w-36 h-36 rounded-full bg-mainBlue"></div>
-              <h2 class="text-md">
-                لوزم ایپسوم متنی است که اختراع شده تا جاهای خالی در طراحی گرافیک
-                پر شود و خالی نمایند متشکرم ازتون لوزم ایپسوم متنی است که اختراع
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Pasha.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                فاطمه پاشاپور
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Roya.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                رویا تندرست
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Farhad.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                فرهادی
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Mahsa.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                مهسا ناطقیان
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Sima.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                سیما سلطانی
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Zereshk.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                فاطمه زرشکی
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Zad.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                صمد زاد
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Masoud.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                مسعود نورجو
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Hojat.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                حجت صمدوند
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Razie.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                راضیه حیدری
+              </h2>
+            </div>
+            <div
+              class="flex flex-col w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Nejati.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                علی نجاتی
+              </h2>
+            </div>
+            <h1
+              class="lg:text-3xl lg:col-span-4 mb-3 font-bold text-xl place-self-end text-darkBlue border-b-8 border-mainYellow pb-4 rounded-xl"
+            >
+              پرسنل اداری اطلس
+            </h1>
+            <div
+              class="flex flex-col lg:col-span-2 w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Parya.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                پریا ثروت جو
+              </h2>
+            </div>
+            <div
+              class="flex flex-col lg:col-span-2 w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Sona.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                صونا صفری
+              </h2>
+            </div>
+            <h1
+              class="lg:text-3xl lg:col-span-4 mb-3 font-bold text-xl place-self-end text-darkBlue border-b-8 border-mainYellow pb-4 rounded-xl"
+            >
+              معلمان مدرسه اطلس
+            </h1>
+            <div
+              class="flex flex-col lg:col-span-2 w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Far.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                فرهود فر
+              </h2>
+            </div>
+            <div
+              class="flex flex-col lg:col-span-2 w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Nasiri.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                سیما نصیری
+              </h2>
+            </div>
+            <div
+              class="flex flex-col lg:col-span-4 w-full px-7 items-center justify-center space-y-6"
+            >
+              <div class="w-36 h-36 overflow-hidden rounded-full bg-mainYellow">
+                <img
+                  src="../assets/images/Neda.webp"
+                  class="object-obtain w-32 transform translate-x-2"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-lg border-b-8 border-mainYellow pb-1 rounded-xl">
+                ندا سرباز وطن
               </h2>
             </div>
           </div>
