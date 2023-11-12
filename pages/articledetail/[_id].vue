b06ec1290cd8aff9a5dcd5ba81c7d49c447ab41c
Greez <imgreez@protonmail.com>
2023-11-12 16:39:41 +0330

    Update [_id].vue

--- pages/articledetail/[_id].vue
+++ pages/articledetail/[_id].vue
@@ -132,7 +132,7 @@
           <h3 class="text-lg text-right">
             {{ article.first_header }}
           </h3>
-          <NuxtLink :to="'articledetail/' + article.id">
+          <NuxtLink :to="'/articledetail/' + article.id">
             <button
               class="px-12 py-3 lg:my-0 text-xl font-bold border-2 items-center border-mainYellow active:bg-mainYellow active:text-white bg-mainYellow hover:bg-white hover:text-darkBlue shadow-md shadow-transparent hover:shadow-mainYellow text-darkBlue transition ease-linear duration-200 flex space-x-2 rounded-md"
             >
