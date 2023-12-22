3b73f18af040eeee8c02ad5a0b73edbbec3fc63f
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-12-22 09:13:18 +0330

    Update

--- pages/courses/FCE.vue
+++ pages/courses/FCE.vue
@@ -1,6 +1,6 @@
 <template>
   <head>
-    <title dir="rtl">دوره های FCE منظومه آموزشی و فرهنگی اصلس</title>
+    <title dir="rtl">دوره های FCE منظومه آموزشی و فرهنگی اطلس</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
   </head>
